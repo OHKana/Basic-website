cfd3266cfa9e89a3e1183a44a78f3cf59912a52b
kona <ummeyhoney01795@gmail.com>
2021-03-06 16:51:17 +0600

    o

--- public/index.php
+++ public/index.php
@@ -31,6 +31,7 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 |
 */
 
+
 require __DIR__.'/../vendor/autoload.php';
 
 /*
@@ -53,3 +54,4 @@ $response = tap($kernel->handle(
 ))->send();
 
 $kernel->terminate($request, $response);
+
